858e899ac9b9b61979a1dee6551dee8baf78f765
BigBagsty <franciscogsf2004@gmail.com>
2025-04-02 14:48:33 +0100

    pipes
    still correcting the pipes

--- srcs/pipes.c
+++ srcs/pipes.c
@@ -12,75 +12,99 @@
 
 #include "../lib/minishell.h"
 
-static void create_pipe(int p_fd[2])
+
+static void create_pipe(int p_fd[2]) 
 {
     if (pipe(p_fd) == -1) 
+    {
+        perror("pipe");
         exit(1);
+    }
 }
 
-static void execute_command(char *cmd, int prev_fd, int p_fd[2], int is_last)
-{
+static void execute_command(char **cmd, int prev_fd, int p_fd[2], int is_last) {
     if (prev_fd != 0) 
     {
-        dup2(prev_fd, 0);
+        if (dup2(prev_fd, STDIN_FILENO) == -1) 
+        {
+            exit(1);
+        }
         close(prev_fd);
     }
     if (!is_last) 
     {
-        dup2(p_fd[1], 1);
+        if (dup2(p_fd[1], STDOUT_FILENO) == -1) 
+        {
+            exit(1);
+        }
         close(p_fd[1]);
     }
     close(p_fd[0]);
-
-    char *argv[] = {cmd, NULL};
-    execve(cmd, argv, NULL);
+    execvp(cmd[0], cmd);
+    perror("execvp");
     exit(1);
 }
 
-static void handle_child_process(char *cmd, int prev_fd, int p_fd[2], int is_last)
+static void handle_child_process(char **cmd, int prev_fd, int p_fd[2], int is_last) 
 {
     pid_t pid = fork();
-    if (pid == -1) 
+    if (pid == -1)
     {
         perror("fork");
         exit(1);
     }
+
     if (pid == 0) 
     {
         execute_command(cmd, prev_fd, p_fd, is_last);
     }
 }
 
-static void handle_parent_process(int p_fd[2], int *prev_fd)
+static void handle_parent_process(int p_fd[2], int *prev_fd) 
 {
     close(p_fd[1]);
-    if (*prev_fd != 0)
+    if (*prev_fd != 0) 
+    {
         close(*prev_fd);
+    }
     *prev_fd = p_fd[0];
 }
 
-void ft_pipe(t_env *cmds)
+void ft_pipe(t_env *cmds) 
 {
     int prev_fd = 0;
     int p_fd[2];
-    int num_cmds;
-    int i;
-    num_cmds = 0;
-    i = 0;
-    while (cmds->arg[num_cmds])
+    int num_cmds = 0;
+    t_env *cmd = cmds->arg;
+    while (cmd) 
     {
         num_cmds++;
+        cmd = cmd->next;
     }
-    while ( i < num_cmds) 
+
+    for (int i = 0; i < num_cmds; i++) 
     {
-        if (i < num_cmds - 1)
+        if (i < num_cmds - 1) 
+        {
             create_pipe(p_fd);
-        handle_child_process(cmds->arg[i], prev_fd, p_fd, i == num_cmds - 1); 
-        if (i < num_cmds - 1)
+        } 
+        else 
+        {
+            p_fd[0] = p_fd[1] = -1;
+        }
+
+        handle_child_process(cmds->arg, prev_fd, p_fd, i == num_cmds - 1);
+
+        if (i < num_cmds - 1) 
+        {
             handle_parent_process(p_fd, &prev_fd);
+        }
+
         cmds = cmds->next;
-        i++;
     }
-    
+    if (prev_fd != 0) 
+    {
+        close(prev_fd);
+    }
     while (wait(NULL) > 0);
 }
