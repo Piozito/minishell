b52f45f819d3fb2bbec2711d35b12328da6b1d6d
BigBagsty <169063777+BigBagsty@users.noreply.github.com>
2025-05-09 13:02:15 +0100

    changed some things in the pipes

--- srcs/pipes.c
+++ srcs/pipes.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipes.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaleixo- <aaleixo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: fragarc2 <fragarc2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 13:30:04 by marvin            #+#    #+#             */
-/*   Updated: 2025/05/09 09:39:24 by aaleixo-         ###   ########.fr       */
+/*   Updated: 2025/05/09 11:57:16 by fragarc2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 static void create_pipe(int p_fd[2])
 {
     if (pipe(p_fd) == -1)
-    {
+	{
         perror("pipe");
         exit(1);
     }
@@ -25,39 +25,34 @@ static void create_pipe(int p_fd[2])
 static void execute_command(t_env *cmd, int prev_fd, int p_fd[2], int is_last)
 {
     if (prev_fd != 0)
-    {
-        if (dup2(prev_fd, STDIN_FILENO) == -1)
-            exit(1);
-        close(prev_fd);
-    }
-    if (!is_last)
-    {
-        if (dup2(p_fd[1], STDOUT_FILENO) == -1)
-            exit(1);
-        close(p_fd[1]);
-    }
-    close(p_fd[0]);
-    check_builtin(cmd);
-    exit(1);
+	{
+	    if (dup2(prev_fd, STDIN_FILENO) == -1)
+	        exit(1);
+	    close(prev_fd);
+	}
+	if (!is_last)
+	{
+    	if (dup2(p_fd[1], STDOUT_FILENO) == -1)
+    	    exit(1);
+   	 	close(p_fd[1]);
+	}
+	apply_fd(cmd);
+    int status = check_builtin(cmd);
+    exit(status);
 }
 
-static void handle_child_process(t_env *cmds, int prev_fd, int p_fd[2], int is_last)
+
+static pid_t handle_child_process(t_env *cmds, int prev_fd, int p_fd[2], int is_last)
 {
     pid_t pid = fork();
     if (pid == -1)
-    {
+	{
         perror("fork");
         exit(1);
     }
     if (pid == 0)
-	{
-		if((ft_isalpha(cmds->cmd[0]) == 0 && cmd_check(cmds) == 0) || cmds->cmd[0] == ' ')
-		{
-			printf("command not found: \"%s\"\n", cmds->cmd);
-			exit(127);
-		}
         execute_command(cmds, prev_fd, p_fd, is_last);
-	}
+    return pid;
 }
 
 static void handle_parent_process(int p_fd[2], int *prev_fd)
@@ -68,40 +63,65 @@ static void handle_parent_process(int p_fd[2], int *prev_fd)
     *prev_fd = p_fd[0];
 }
 
-void ft_pipe(t_env *cmds)
+int ft_pipe(t_env *cmds)
 {
     int prev_fd = 0;
     int p_fd[2];
     int num_cmds = 0;
-	int i;
+    int i = 0;
     t_env *cmd;
+    pid_t last_pid = -1;
+    int status, exit_code = 0;
 
-	i = 0;
     if (cmds == NULL)
 	{
         fprintf(stderr, "Error: cmds is NULL\n");
-        return;
+        return 1;
     }
+
     cmd = cmds;
     while (cmd)
-    {
+	{
         num_cmds++;
         cmd = cmd->next;
     }
+
     cmd = cmds;
     while (i < num_cmds)
-    {
+	{
+		if (cmd_check(cmds) == 0 && num_cmds == 1)
+		{
+			int status = check_builtin(cmds);
+    		return status;
+		}
         if (i < num_cmds - 1)
             create_pipe(p_fd);
         else
             p_fd[0] = p_fd[1] = -1;
-        handle_child_process(cmd, prev_fd, p_fd, i == num_cmds - 1);
+
+        pid_t pid = handle_child_process(cmd, prev_fd, p_fd, i == num_cmds - 1);
+        if (i == num_cmds - 1)
+            last_pid = pid;
+
         if (i < num_cmds - 1)
             handle_parent_process(p_fd, &prev_fd);
+
         cmd = cmd->next;
-		i++;
+        i++;
     }
     if (prev_fd != 0)
-		close(prev_fd);
-    while (wait(NULL) > 0);
+        close(prev_fd);
+
+    pid_t wpid;
+    while ((wpid = wait(&status)) > 0)
+	{
+        if (wpid == last_pid)
+		{
+            if (WIFEXITED(status))
+                exit_code = WEXITSTATUS(status);
+            else
+                exit_code = 1;
+        }
+    }
+    return exit_code;
 }
