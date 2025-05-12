94c46146c78752c8672c6f6a581ad1c00579d36b
Pio <77726736+Piozito@users.noreply.github.com>
2025-05-12 12:38:21 +0100

    Mostly norminette fixes

--- srcs/utils2.c
+++ srcs/utils2.c
@@ -6,231 +6,248 @@
 /*   By: aaleixo- <aaleixo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 17:39:06 by aaleixo-          #+#    #+#             */
-/*   Updated: 2025/05/12 08:54:31 by aaleixo-         ###   ########.fr       */
+/*   Updated: 2025/05/12 10:55:57 by aaleixo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/minishell.h"
 
-static int is_separator(char ch, char c, int quote, int dquote)
+static int	is_separator(char ch, char c, int quote, int dquote)
 {
-	if(c == ' ')
-		return !quote && !dquote && (ch == c || ch == '\t' || ch == '\0');
-	return !quote && !dquote && (ch == c || ch == '\0');
+	if (c == ' ')
+		return (!quote && !dquote && (ch == c || ch == '\t' || ch == '\0'));
+	return (!quote && !dquote && (ch == c || ch == '\0'));
 }
 
-char *ft_find_closing_quote(const char *str, int start, char quote)
+char	*ft_find_closing_quote(const char *str, int start, char quote)
 {
-	int i = start;
+	int	i;
+
+	i = start;
 	while (str[i] != '\0')
 	{
 		if (str[i] == quote)
-			return (char *)(str + i);
+			return ((char *)(str + i));
 		i++;
 	}
 	return (NULL);
 }
-static int word_count(const char *s, char c)
-{
-    int i = 0;
-    int count = 0;
-    int quote = 0;
-    int dquote = 0;
 
-    while (s[i])
-    {
-        if (s[i] == '\'' && !dquote)
-        {
-            if (quote)
-                quote = 0;
-            else if (ft_find_closing_quote(s, i + 1, '\''))
-                quote = 1;
-            i++;
-            continue;
-        }
-        else if (s[i] == '\"' && !quote)
-        {
-            if (dquote)
-                dquote = 0;
-            else if (ft_find_closing_quote(s, i + 1, '\"'))
-                dquote = 1;
-            i++;
-            continue;
-        }
-        else if (is_separator(s[i], c, quote, dquote))
-        {
-            if (!is_separator(s[i + 1], c, quote, dquote))
-                count++;
-        }
-        i++;
-    }
-    if (s[0] && !is_separator(s[0], c, 0, 0))
-        count++;
-    return count;
+static int	word_count(const char *s, char c)
+{
+	int	i;
+	int	count;
+	int	quote;
+	int	dquote;
+
+	i = 0;
+	count = 0;
+	quote = 0;
+	dquote = 0;
+	while (s[i])
+	{
+		if (s[i] == '\'' && !dquote)
+		{
+			if (quote)
+				quote = 0;
+			else if (ft_find_closing_quote(s, i + 1, '\''))
+				quote = 1;
+			i++;
+			continue ;
+		}
+		else if (s[i] == '\"' && !quote)
+		{
+			if (dquote)
+				dquote = 0;
+			else if (ft_find_closing_quote(s, i + 1, '\"'))
+				dquote = 1;
+			i++;
+			continue ;
+		}
+		else if (is_separator(s[i], c, quote, dquote))
+		{
+			if (!is_separator(s[i + 1], c, quote, dquote))
+				count++;
+		}
+		i++;
+	}
+	if (s[0] && !is_separator(s[0], c, 0, 0))
+		count++;
+	return (count);
 }
 
-
-char **pipe_check(t_env *cmds, const char *input)
+char	**pipe_check(t_env *cmds, const char *input)
 {
-    int i = 0;
-    int pipe = 0;
-    if (input[0] == '|')
-    {
-        printf("pipe: parsing error.\n");
-        return NULL;
-    }
-    while (input[i] != '\0')
-    {
-        if (input[i] == '\'' || input[i] == '\"')
-        {
-            if (input[i] == '\'')
-            {
-                if (ft_find_closing_quote(input, i + 1, '\''))
-                {
-					pipe = 0;
-                    i++;
-                    while (input[i] != '\'')
-                        i++;
-                }
-            }
-            else if (input[i] == '\"')
-            {
-                if (ft_find_closing_quote(input, i + 1, '\"'))
-                {
-					pipe = 0;
-                    i++;
-                    while (input[i] != '\"')
-                        i++;
-                }
-            }
-        }
-        else if (input[i] == '|')
-        {
-            if (pipe == 1)
-            {
-                printf("pipe: parsing error.\n");
-                return NULL;
-            }
-            else
-                pipe = 1;
-        }
-        else if (input[i] != ' ' && input[i] != '|' && input[i] != '\t' && input[i] != '\0')
-            pipe = 0;
-        i++;
-    }
-    if (pipe == 1)
-    {
-        printf("pipe: parsing error.\n");
-        return NULL;
-    }
-    return (ft_split_quotes(cmds, input, '|', 0));
-}
+	int	i;
+	int	pipe;
 
-static char *extract_word(t_env *cmd, const char *s, int *index, char delimiter, int del)
-{
-    char *result;
-    int allocated_size = 1024;
-    int i = 0;
-    int quote = 0;
-    int dquote = 0;
-
-    result = (char *)malloc(allocated_size * sizeof(char));
-    if (!result)
+	i = 0;
+	pipe = 0;
+	if (input[0] == '|')
+	{
+		printf("pipe: parsing error.\n");
 		return (NULL);
-	while (s[*index] && !is_separator(s[*index], delimiter, quote, dquote))	
-    {
-		if (!quote && s[*index] == '$' && ft_isprint(s[*index + 1]) == 1)
-        {
-            ft_expand_variable(cmd, s, index, &result, &i);
-			(*index)++;
-            continue;
-        }
-        if (s[*index] == '\'' && !dquote)
-        {
-            if (quote)
-            {
-                quote = 0;
-                if(del == 0)
-					result[i++] = s[(*index)++];
-				else if(del == 1)
-					(*index)++;
-                continue;
-            }
-            if (ft_find_closing_quote(s, *index + 1, '\''))
-            {
-                quote = 1;
-                if(del == 0)
-					result[i++] = s[(*index)++];
-				else if(del == 1)
-					(*index)++;
-                continue;
-            }
-            result[i++] = s[(*index)++];
-        }
-        else if (s[*index] == '\"' && !quote)
-        {
-            if (dquote)
-            {
-                dquote = 0;
-				if(del == 0)
-					result[i++] = s[(*index)++];
-				else if(del == 1)
-					(*index)++;
-                continue;
-            }
-            if (ft_find_closing_quote(s, *index + 1, '\"'))
-            {
-                dquote = 1;
-				if(del == 0)
-					result[i++] = s[(*index)++];
-				else if(del == 1)
-					(*index)++;
-                continue;
-            }
-            result[i++] = s[(*index)++];
-        }
-        else
-        {
-            result[i++] = s[*index];
-            (*index)++;
-        }
-		if (i >= allocated_size - 1)
-        {
-            char *new_result = (char *)malloc(2 * allocated_size * sizeof(char));
-            if (!new_result)
-            {
-                free(result);
-                return (NULL);
-            }
-            ft_strlcpy(new_result, result, allocated_size);
-            free(result);
-            result = new_result;
-            allocated_size *= 2;
-        }
-    }
-    result[i] = '\0';
-    return result;
+	}
+	while (input[i] != '\0')
+	{
+		if (input[i] == '\'' || input[i] == '\"')
+		{
+			if (input[i] == '\'')
+			{
+				if (ft_find_closing_quote(input, i + 1, '\''))
+				{
+					pipe = 0;
+					i++;
+					while (input[i] != '\'')
+						i++;
+				}
+			}
+			else if (input[i] == '\"')
+			{
+				if (ft_find_closing_quote(input, i + 1, '\"'))
+				{
+					pipe = 0;
+					i++;
+					while (input[i] != '\"')
+						i++;
+				}
+			}
+		}
+		else if (input[i] == '|')
+		{
+			if (pipe == 1)
+			{
+				printf("pipe: parsing error.\n");
+				return (NULL);
+			}
+			else
+				pipe = 1;
+		}
+		else if (input[i] != ' ' && input[i] != '|' && input[i] != '\t' && input[i] != '\0')
+			pipe = 0;
+		i++;
+	}
+	if (pipe == 1)
+	{
+		printf("pipe: parsing error.\n");
+		return (NULL);
+	}
+	return (ft_split_quotes(cmds, input, '|', 0));
 }
 
-char **ft_split_quotes(t_env *cmd, const char *s, char delimiter, int del)
+static char	*extract_word(t_env *cmd, const char *s, int *index, char delimiter, int del)
 {
-	char **result;
-    int i = 0;
-    int index = 0;
-    int words = word_count(s, delimiter);
+	char	*result;
+	char	*new_result;
+	int		allocated_size;
+	int		i;
+	int		quote;
+	int		dquote;
 
-    if (!s)
-        return (NULL);
-    result = (char **)malloc((words + 1) * sizeof(char *));
-    if (!result)
-        return (NULL);
-    while (i < words)
-    {
-        while (s[index] && is_separator(s[index], delimiter, 0, 0))
-            index++;
-        if (s[index] && !is_separator(s[index], delimiter, 0, 0))
-            result[i++] = extract_word(cmd, s, &index, delimiter, del);
-    }
-    result[i] = NULL;
-    return (result);
+	allocated_size = 1024;
+	i = 0;
+	quote = 0;
+	dquote = 0;
+	result = (char *)malloc(allocated_size * sizeof(char));
+	if (!result)
+		return (NULL);
+	while (s[*index] && !is_separator(s[*index], delimiter, quote, dquote))
+	{
+		if (!quote && s[*index] == '$' && ft_isprint(s[*index + 1]) == 1)
+		{
+			ft_expand_variable(cmd, s, index, &result, &i);
+			(*index)++;
+			continue ;
+		}
+		if (s[*index] == '\'' && !dquote)
+		{
+			if (quote)
+			{
+				quote = 0;
+				if (del == 0)
+					result[i++] = s[(*index)++];
+				else if (del == 1)
+					(*index)++;
+				continue ;
+			}
+			if (ft_find_closing_quote(s, *index + 1, '\''))
+			{
+				quote = 1;
+				if (del == 0)
+					result[i++] = s[(*index)++];
+				else if (del == 1)
+					(*index)++;
+				continue ;
+			}
+			result[i++] = s[(*index)++];
+		}
+		else if (s[*index] == '\"' && !quote)
+		{
+			if (dquote)
+			{
+				dquote = 0;
+				if (del == 0)
+					result[i++] = s[(*index)++];
+				else if (del == 1)
+					(*index)++;
+				continue ;
+			}
+			if (ft_find_closing_quote(s, *index + 1, '\"'))
+			{
+				dquote = 1;
+				if (del == 0)
+					result[i++] = s[(*index)++];
+				else if (del == 1)
+					(*index)++;
+				continue ;
+			}
+			result[i++] = s[(*index)++];
+		}
+		else
+		{
+			result[i++] = s[*index];
+			(*index)++;
+		}
+		if (i >= allocated_size - 1)
+		{
+			new_result = (char *)malloc(2 * allocated_size * sizeof(char));
+			if (!new_result)
+			{
+				free(result);
+				return (NULL);
+			}
+			ft_strlcpy(new_result, result, allocated_size);
+			free(result);
+			result = new_result;
+			allocated_size *= 2;
+		}
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char	**ft_split_quotes(t_env *cmd, const char *s, char delimiter, int del)
+{
+	char	**result;
+	int		i;
+	int		index;
+	int		words;
+
+	i = 0;
+	index = 0;
+	words = word_count(s, delimiter);
+	if (!s)
+		return (NULL);
+	result = (char **)malloc((words + 1) * sizeof(char *));
+	if (!result)
+		return (NULL);
+	while (i < words)
+	{
+		while (s[index] && is_separator(s[index], delimiter, 0, 0))
+			index++;
+		if (s[index] && !is_separator(s[index], delimiter, 0, 0))
+			result[i++] = extract_word(cmd, s, &index, delimiter, del);
+	}
+	result[i] = NULL;
+	return (result);
 }
--- srcs/utils3.c
+++ srcs/utils3.c
@@ -6,53 +6,56 @@
 /*   By: aaleixo- <aaleixo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 15:26:40 by aaleixo-          #+#    #+#             */
-/*   Updated: 2025/05/09 20:21:42 by aaleixo-         ###   ########.fr       */
+/*   Updated: 2025/05/12 12:36:32 by aaleixo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/minishell.h"
 
-void ft_debug(t_env *cmd)
+void	ft_debug(t_env *cmd)
 {
-	if(cmd->path)
+	int	i;
+
+	i = 0;
+	if (cmd->path)
 		printf("\033[0;32mpath is available.\033[0m\n");
 	else
 		printf("\033[0;31mpath has failed.\033[0m\n");
-	if(cmd->env)
+	if (cmd->env)
 		printf("\033[0;32menv is available.\033[0m\n");
 	else
 		printf("\033[0;31menv has failed.\033[0m\n");
-	if(cmd->exp)
+	if (cmd->exp)
 		printf("\033[0;32mexport is available.\033[0m\n");
 	else
 		printf("\033[0;31mexport has failed.\033[0m\n");
-    printf("-------------------------\n");
-	printf("Command: %s\n", cmd->cmd);
-    if (cmd->arg && cmd->arg[0] != NULL)
-    {
-        printf("Arguments:\n");
-        for (int i = 0; cmd->arg[i] != NULL; i++)
-        {
-            printf("  Arg[%d]: %s\n", i, cmd->arg[i]);
-        }
-    }
-    else
-        printf("Arguments: None\n");
-    printf("-------------------------\n");
+	printf("-------------------------\nCommand: %s\n", cmd->cmd);
+	if (cmd->arg && cmd->arg[0] != NULL)
+	{
+		printf("Arguments:\n");
+		while (cmd->arg[i] != NULL)
+		{
+			printf("	Arg[%d]: %s\n", i, cmd->arg[i]);
+			i++;
+		}
+	}
+	else
+		printf("Arguments: None\n");
+	printf("-------------------------\n");
 }
 
-void pop(t_env *cmds, int i)
+void	pop(t_env *cmds, int i)
 {
-	pid_t pid;
-	int saved_stdin;
-	int saved_stdout;
+	pid_t	pid;
+	int		saved_stdin;
+	int		saved_stdout;
 
 	if (cmd_check(cmds) == 0)
 	{
 		saved_stdin = dup(0);
 		saved_stdout = dup(1);
 		apply_fd(cmds);
-		if(i == 0)
+		if (i == 0)
 			check_builtin(cmds);
 		dup2(saved_stdin, 0);
 		dup2(saved_stdout, 1);
@@ -61,29 +64,29 @@ void pop(t_env *cmds, int i)
 		return ;
 	}
 	pid = fork();
-	if (pid == 0) 
+	if (pid == 0)
 	{
 		apply_fd(cmds);
-		if(i == 0)
+		if (i == 0)
 			check_builtin(cmds);
-	    exit(1);
-	} 
+		exit(1);
+	}
 	else if (pid > 0)
-	    waitpid(pid, NULL, 0);
+		waitpid(pid, NULL, 0);
 	else
-	    perror("fork failed");
-	return ; 
+		perror("fork failed");
+	return ;
 }
 
-void check_errors(t_env *cmds)
+void	check_errors(t_env *cmds)
 {
-	t_env *temp;
+	t_env	*temp;
 
 	temp = cmds;
-	while(temp)
+	while (temp)
 	{
 		temp->path = my_get_path(temp);
-		if(temp->path == NULL)
+		if (temp->path == NULL)
 			printf("command not found: \"%s\"\n", temp->cmd);
 		temp = temp->next;
 	}
@@ -96,8 +99,8 @@ char	*ft_strstr(const char *big, const char *little)
 
 	if (*little == '\0')
 		return ((char *)big);
-	if(*big == '\0')
-		return NULL;
+	if (*big == '\0')
+		return (NULL);
 	i = 0;
 	while (big[i])
 	{
