4e7b0562ba505f7d334050b6814e347f21ccb0f0
BigBagsty <169063777+BigBagsty@users.noreply.github.com>
2025-05-14 15:25:55 +0100

    exit corrected

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaleixo- <aaleixo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: fragarc2 <fragarc2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 19:56:36 by marvin            #+#    #+#             */
-/*   Updated: 2025/05/14 09:51:36 by aaleixo-         ###   ########.fr       */
+/*   Updated: 2025/05/14 15:22:33 by fragarc2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,42 +15,42 @@
 static int	is_numeric(const char *str)
 {
 	int	i;
-	int num;
 
 	i = 0;
-	num = ft_atoi(str);
 	while (str[i])
 	{
-		if (!ft_isdigit(num))
+		if (!ft_isdigit(str[i]))
 			return (0);
 		i++;
 	}
 	return (1);
 }
-
-int	ft_exit(t_env *cmds)
+int num_ex(t_env *cmds)
 {
 	int	exit_status;
 
-	if(cmds->arg[0] == NULL)
-		exit(0);
-	if (!is_numeric(cmds->arg[0]))
+	exit_status = ft_atoi(cmds->arg[0]);
+	return (exit_status % 256);
+}
+
+int	ft_exit(t_env *cmds)
+{
+	if (cmds->arg[1] == NULL && is_numeric(cmds->arg[0]) == 1)
 	{
 		if (cmds->arg[0])
-		{
-			exit_status = ft_atoi(cmds->arg[0]);
-			if(exit_status > 255 || exit_status < 0)
-			{
-				write(cmds->fd, "exit: Error codes must be less than 256.\n", 42);
-				return (255);
-			}
-			exit(exit_status % 256);
-		}
+			exit(num_ex(cmds));
 		exit(0);
 	}
+	else if (cmds->arg[0] && cmds->arg[1] == NULL)
+	{
+		write(cmds->fd, "non numeric arg \n", 18);
+		exit (2);
+	}
+	else if(!cmds->arg[0])
+		exit(0);
 	else
 	{
-		write(cmds->fd, "exit: exit only accepts a positive integer less than 256 and no flags.\n", 72);
+		write(cmds->fd, "too many arguments \n", 20);
 		return (128);
 	}
 }
