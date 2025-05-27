6606e3f6130730ebeeaea5ebe3221e49440b50c6
Pio <77726736+Piozito@users.noreply.github.com>
2025-05-27 17:17:53 +0100

    norminette expand

--- srcs/expand.c
+++ srcs/expand.c
@@ -12,104 +12,52 @@
 
 #include "../lib/minishell.h"
 
-char *env_expander(t_env *cmds, char *var_name)
+void	exp_line(const char *input, size_t *cap, char **res, t_env *cmd)
 {
-	int i = 0;
-	int j = 0;
-	char *res = NULL;
-	while (cmds->env[i])
-	{
-		j = 0;
-		if(ft_strncmp(var_name, cmds->env[i], ft_strlen(var_name)) == 0)
-		{
-			res = ft_strtrim(ft_strchr(cmds->env[i], '='), "=");
-			while(cmds->env[i][j] != '=')
-				j++;
-			if(ft_strncmp(var_name, cmds->env[i], j) != 0)
-				return ft_strdup("");
-			if(res == NULL)
-				return ft_strdup("");
-			return res;
-		}
-		i++;
-	}
-	return ft_strdup("");
-}
+	size_t	i;
+	size_t	j;
+	int		quote;
 
-char *expand_string_variables(t_env *cmd, const char *input)
-{
-	size_t result_capacity = ft_strlen(input) + 1024;
-	char *result = malloc(result_capacity);
-	size_t result_len = 0;
-	char *expanded;
-
-	size_t i = 0;
-	int quote = 0;
-	int dquote = 0;
-	char *var_name = NULL;
-	size_t var_index = 0;
-	if (!result)
-		return NULL;
+	i = 0;
+	j = 0;
+	quote = 0;
 	while (input[i])
 	{
-		if (input[i] == '\'' && !dquote && ft_find_closing_quote(input, i + 1, '\''))
-		{
-			quote = !quote;
-			result[result_len++] = input[i++];
-		}
-		else if (input[i] == '\"' && !quote  && ft_find_closing_quote(input, i + 1, '\"'))
-		{
-			dquote = !dquote;
-			result[result_len++] = input[i++];
-		}
-		else if (!quote && input[i] == '$' && input[i + 1] && ft_isprint(input[i + 1]) && input[i + 1] != ' ' && input[i + 1] != '\t')
-		{
-			i++;
-			var_index = 0;
-			while (input[i + var_index] && input[i + var_index] != ' ' && input[i + var_index] != '$' && 
-				   input[i + var_index] != '\t' && input[i + var_index] != '\"' && input[i + var_index] != '\'')
-				var_index++;
-			var_name = (char *)malloc((var_index + 1) * sizeof(char));
-			if (!var_name)
-			{
-				free(result);
-				return NULL;
-			}
-			var_index = 0;
-			while (input[i] && input[i] != ' ' && input[i] != '$' && 
-				   input[i] != '\t' && input[i] != '\"' && input[i] != '\'')
-				var_name[var_index++] = input[i++];
-			var_name[var_index] = '\0';
-			if (var_name[0] == '?' && var_name[1] == '\0')
-				expanded = ft_itoa(cmd->exit_status);
-			else
-				expanded = env_expander(cmd, var_name);
-			free(var_name);
-			if (expanded)
-			{
-				if (result_len + ft_strlen(expanded) + 1 >= result_capacity)
-					result = ft_strrealloc(result, &result_capacity);
-				ft_strlcpy(result + result_len, expanded, ft_strlen(expanded) + 1);
-				result_len += ft_strlen(expanded);
-				free(expanded);
-			}
-		}
-		else if(result_len + 2 >= result_capacity)
-			result = ft_strrealloc(result, &result_capacity);
+		if (input[i] == '\'' || input[i] == '\"')
+			quote = var_quote(input, &i, quote);
+		if (quote != '\'' && input[i] == '$' && input[i + 1]
+			&& ft_isprint(input[i + 1]) && input[i + 1] != ' '
+			&& input[i + 1] != '\t')
+			var_joiner(var_expander(input, &i, cmd), res, cap, &j);
 		else
-			result[result_len++] = input[i++];
+			(*res)[j++] = input[i++];
+		if (j + 2 >= (*cap))
+			(*res) = ft_strrealloc((*res), cap);
 	}
-	result[result_len] = '\0';
-	return result;
+	(*res)[j] = '\0';
 }
 
-void expand_variables(t_env *cmd, char **result)
+char	*expand_string_variables(t_env *cmd, const char *input)
 {
-	size_t i = 0;
-	char *expanded;
+	size_t	cap;
+	char	*res;
 
+	cap = ft_strlen(input) + 1024;
+	res = malloc(cap);
+	if (!res)
+		return (NULL);
+	exp_line(input, &cap, &res, cmd);
+	return (res);
+}
+
+void	expand_variables(t_env *cmd, char **result)
+{
+	size_t	i;
+	char	*expanded;
+
+	i = 0;
 	if (!result)
-		return;
+		return ;
 	while (result[i] != NULL)
 	{
 		expanded = expand_string_variables(cmd, result[i]);
---
+++ srcs/expand_utils.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaleixo- <aaleixo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 17:11:20 by aaleixo-          #+#    #+#             */
+/*   Updated: 2025/05/27 17:11:46 by aaleixo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../lib/minishell.h"
+
+char	*env_expander(t_env *cmds, char *var_name)
+{
+	int		i;
+	int		j;
+	char	*res;
+
+	i = 0;
+	while (cmds->env[i])
+	{
+		j = 0;
+		if (ft_strncmp(var_name, cmds->env[i], ft_strlen(var_name)) == 0)
+		{
+			res = ft_strtrim(ft_strchr(cmds->env[i], '='), "=");
+			while (cmds->env[i][j] != '=')
+				j++;
+			if (ft_strncmp(var_name, cmds->env[i], j) != 0)
+				return (ft_strdup(""));
+			if (res == NULL)
+				return (ft_strdup(""));
+			return (res);
+		}
+		i++;
+	}
+	return (ft_strdup(""));
+}
+
+char	*var_getter(const char *input, size_t *i)
+{
+	int		var_index;
+	char	*var_name;
+
+	(*i)++;
+	var_index = 0;
+	while (input[(*i) + var_index] && input[(*i) + var_index] != ' '
+		&& input[(*i) + var_index] != '$'
+		&& input[(*i) + var_index] != '\t' && input[(*i) + var_index] != '\"'
+		&& input[(*i) + var_index] != '\'')
+		var_index++;
+	var_name = (char *)malloc((var_index + 1) * sizeof(char));
+	if (!var_name)
+		return (NULL);
+	var_index = 0;
+	while (input[(*i)] && input[(*i)] != ' ' && input[(*i)] != '$'
+		&& input[(*i)] != '\t' && input[(*i)] != '\"' && input[(*i)] != '\'')
+		var_name[var_index++] = input[(*i)++];
+	var_name[var_index] = '\0';
+	return (var_name);
+}
+
+char	*var_expander(const char *input, size_t *i, t_env *cmd)
+{
+	char	*var_name;
+	char	*expanded;
+
+	var_name = var_getter(input, i);
+	if (var_name[0] == '?' && var_name[1] == '\0')
+		expanded = ft_itoa(cmd->exit_status);
+	else
+		expanded = env_expander(cmd, var_name);
+	free(var_name);
+	return (expanded);
+}
+
+void	var_joiner(char *exp, char **res, size_t *cap, size_t *len)
+{
+	if (exp)
+	{
+		if ((*len) + ft_strlen(exp) + 1 >= (*cap))
+			(*res) = ft_strrealloc((*res), &(*cap));
+		ft_strlcpy((*res) + (*len), exp, ft_strlen(exp) + 1);
+		(*len) += ft_strlen(exp);
+		free(exp);
+	}
+}
+
+int	var_quote(const char *input, size_t *i, int quote)
+{
+	if (!quote && ft_find_closing_quote(input, *i + 1, input[*i]))
+		quote = input[*i];
+	else if (quote == input[*i])
+		quote = 0;
+	return (quote);
+}
