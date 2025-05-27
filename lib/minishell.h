526c7447154e0a248c79f9d15d90b62344d006c8
Pio <77726736+Piozito@users.noreply.github.com>
2025-05-27 15:25:45 +0100

    norminette

--- lib/minishell.h
+++ lib/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fragarc2 <fragarc2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aaleixo- <aaleixo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 12:00:49 by aaleixo-          #+#    #+#             */
-/*   Updated: 2025/05/26 15:26:30 by fragarc2         ###   ########.fr       */
+/*   Updated: 2025/05/27 15:25:11 by aaleixo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,24 +64,32 @@ int		apply_fd(t_env *cmds);
 int		cmd_check(t_env *cmds);
 int		ft_exec(t_env *command);
 int		pop(t_env *cmds, int i);
+int		check_heredoc(t_env *cmds);
 int		apply_heredoc(t_env *cmds);
 int		check_builtin(t_env *cmds);
 int		wait_for_it(pid_t last_pid);
+int		check_redirs(char **result);
 int		executable_check(t_env *cmds);
 int		is_chr(const char *str, int c);
+int		word_count_m(const char *s, char c);
 int		parsing(t_env *cmd, const char *input);
 int		handle_parent(pid_t pid, t_env *command);
+int		is_separator(char ch, char c, int quote, int dquote);
 int		general_error(char *str, int free, int ex, t_env *cmds);
-int		check_heredoc(t_env *cmds, const char *s, size_t *index);
+int		handle_quote(const char *s, size_t *index, int *quote_flag);
+int		extract_helper(char **result, size_t *allocated_size, size_t i);
+int		word_count_helper(const char *s, size_t *i, int *quote, char c);
+int		handle_redir(const char *s, size_t *index, char *result, size_t *i);
 char	*my_get_path(t_env *cmds);
+char	*get_file(t_env *cmds, size_t i);
 char	**deep_copy_environ(char **environ);
 char	**update_env(char *arg, char **env);
-char	*get_file(const char *s, size_t *index);
 char	*env_expander(t_env *cmds, char *var_name);
 char	**pipe_check(t_env *cmds, const char *input);
 char	**new_env_maker(char **env, int j, char *arg);
 char	*ft_strrealloc(char *result, size_t *size_ptr);
 char	*ft_strstr(const char *big, const char *little);
+char	*extract_word(const char *s, size_t *index, char del);
 char	*expand_string_variables(t_env *cmd, const char *input);
 char	**ft_split_quotes(t_env *cmd, const char *s, char delimiter);
 char	*ft_find_closing_quote(const char *str, int start, char quote);
@@ -94,6 +102,7 @@ void	ex_refresh(t_env *env, int i);
 void	free_subtokens(char **subtokens);
 void	remove_args(char **args, int pos);
 void	free_env(char **array, char **env);
+void	remove_closed_quotes(char **result);
 void	expand_variables(t_env *cmd, char **result);
 void	pipes_handler(t_env *cmds, const char *input);
 void	pipo(t_env *cmd, int num_cmds, pid_t last_pid);
