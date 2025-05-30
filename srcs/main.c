a80f496e92648f81371f5cec900cbfd7a72fbddc
Pio <77726736+Piozito@users.noreply.github.com>
2025-05-30 14:10:02 +0100

    norm main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,61 +6,11 @@
 /*   By: aaleixo- <aaleixo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 16:11:53 by aaleixo-          #+#    #+#             */
-/*   Updated: 2025/05/23 18:12:39 by aaleixo-         ###   ########.fr       */
+/*   Updated: 2025/05/30 14:02:27 by aaleixo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/minishell.h"
-
-void	ft_handler(int sig)
-{
-	(void)sig;
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	write(1, "\n", 1);
-	rl_redisplay();
-}
-
-int	check_builtin(t_env *cmds)
-{
-	if(cmds->cmd == NULL)
-		return 127;
-	if (ft_strcmp(cmds->cmd, "echo") == 0)
-		return ft_echo(cmds);
-	else if (ft_strcmp(cmds->cmd, "pwd") == 0)
-		return ft_pwd(cmds);
-	else if (ft_strcmp(cmds->cmd, "cd") == 0)
-		return ft_cd(cmds);
-	else if (ft_strcmp(cmds->cmd, "env") == 0)
-		return ft_env(cmds);
-	else if (ft_strcmp(cmds->cmd, "exit") == 0)
-		return ft_exit(cmds);
-	else if (ft_strcmp(cmds->cmd, "unset") == 0)
-		return ft_unset(cmds);
-	else if (ft_strcmp(cmds->cmd, "export") == 0)
-		return ft_export(cmds);
-	else
-		return ft_exec(cmds);
-}
-
-void	ft_cmds_free(t_env *cmds)
-{
-	int	i;
-
-	while(cmds)
-	{
-		if (cmds->cmd)
-			free(cmds->cmd);
-		if (cmds->arg)
-		{
-			i = 0;
-			while (cmds->arg[i])
-				free(cmds->arg[i++]);
-			free(cmds->arg);
-		}
-		cmds = cmds->next;
-	}
-}
 
 void	check_input(char *input)
 {
@@ -73,55 +23,38 @@ void	check_input(char *input)
 		input[0] = '\0';
 }
 
-int general_error(char *str, int free, int ex, t_env *cmds)
+void	free_env(char **array, char **env)
 {
-	if(free == 1)
-		ft_cmds_free(cmds);
-	ft_putstr_fd(str, 1);
-	ft_putstr_fd("\n", 1);
-	if(ex == 1)
-		exit(1);
-	return 1;
-}
-
-void free_env(char **array, char **env)
-{
-	int i;
+	int	i;
 
 	i = 0;
-	while(env[i] != NULL)
+	while (env[i] != NULL)
 		i++;
-	if(i == 0)
+	if (i == 0)
 		return (free(array));
 	else
 	{
 		i = 0;
-		while(array[i] != NULL)
+		while (array[i] != NULL)
 			free(array[i++]);
 		free(array);
 	}
 }
 
-int	main(int argc, char **argv, char **env)
+void	main_loop(t_env *cmds)
 {
 	char	*input;
-	t_env	*cmds;
 
-	(void)argv;
-	if(argc != 1)
-		general_error("Minishell doesn't take args", 0, 1, NULL);
-	signal(SIGINT, ft_handler);
-	signal(SIGQUIT, SIG_IGN);
-	cmds = (t_env *)malloc(sizeof(t_env));
-	cmds->exp = deep_copy_environ(env);
-	cmds->env = deep_copy_environ(env);
-	cmds->exit_status = 0;
 	while (1)
 	{
 		initialize_cmd(cmds, NULL, 1);
+		signal(SIGINT, ft_handler);
+		signal(SIGQUIT, SIG_IGN);
 		input = readline("./minishell: ");
+		signal(SIGINT, ft_noint_handler);
+		signal(SIGQUIT, ft_noint_handler);
 		if (input == NULL)
-			break;
+			break ;
 		check_input(input);
 		if (*input != '\0')
 		{
@@ -132,10 +65,24 @@ int	main(int argc, char **argv, char **env)
 		ft_cmds_free(cmds);
 		duping(cmds);
 	}
-	printf("exit\n");
+	write(2, "exit\n", 5);
+	free(input);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	t_env	*cmds;
+
+	(void)argv;
+	if (argc != 1)
+		general_error("Minishell doesn't take args", 0, 1, NULL);
+	cmds = (t_env *)malloc(sizeof(t_env));
+	cmds->exp = deep_copy_environ(env);
+	cmds->env = deep_copy_environ(env);
+	cmds->exit_status = 0;
+	main_loop(cmds);
 	free_env(cmds->env, env);
 	free_env(cmds->exp, env);
 	duping(cmds);
-	free(input);
 	rl_clear_history();
 }
---
+++ srcs/signals.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaleixo- <aaleixo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 13:49:43 by aaleixo-          #+#    #+#             */
+/*   Updated: 2025/05/30 14:01:35 by aaleixo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../lib/minishell.h"
+
+void	ft_cmds_free(t_env *cmds)
+{
+	int	i;
+
+	while (cmds)
+	{
+		if (cmds->cmd)
+			free(cmds->cmd);
+		if (cmds->arg)
+		{
+			i = 0;
+			while (cmds->arg[i])
+				free(cmds->arg[i++]);
+			free(cmds->arg);
+		}
+		cmds = cmds->next;
+	}
+}
+
+void	ft_handler(int sig)
+{
+	(void)sig;
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	write(1, "\n", 1);
+	rl_redisplay();
+}
+
+void	ft_noint_handler(int sig)
+{
+	if (sig == SIGQUIT)
+	{
+		if (isatty(0))
+			write(1, "Quit (core dumped)\n", 19);
+	}
+	else if (sig == SIGINT)
+	{
+		if (isatty(0))
+			write(1, "\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+	}
+}
+
+int	general_error(char *str, int free, int ex, t_env *cmds)
+{
+	if (free == 1)
+		ft_cmds_free(cmds);
+	ft_putstr_fd(str, 1);
+	ft_putstr_fd("\n", 1);
+	if (ex == 1)
+		exit(1);
+	return (1);
+}
+
+int	check_builtin(t_env *cmds)
+{
+	if (cmds->cmd == NULL)
+		return (127);
+	if (ft_strcmp(cmds->cmd, "echo") == 0)
+		return (ft_echo(cmds));
+	else if (ft_strcmp(cmds->cmd, "pwd") == 0)
+		return (ft_pwd(cmds));
+	else if (ft_strcmp(cmds->cmd, "cd") == 0)
+		return (ft_cd(cmds));
+	else if (ft_strcmp(cmds->cmd, "env") == 0)
+		return (ft_env(cmds));
+	else if (ft_strcmp(cmds->cmd, "exit") == 0)
+		return (ft_exit(cmds));
+	else if (ft_strcmp(cmds->cmd, "unset") == 0)
+		return (ft_unset(cmds));
+	else if (ft_strcmp(cmds->cmd, "export") == 0)
+		return (ft_export(cmds));
+	else
+		return (ft_exec(cmds));
+}
